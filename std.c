3212671f5266934a6fdd7f8a192ff87fbd3a3405
Matthew Hinkle <matthewhinkle@live.com>
2025-05-03 19:20:29 -0700

    Define initial string API. Implement string appending.

--- std.c
+++ std.c
@@ -6,32 +6,32 @@
 #include <stdlib.h>
 
 extern void * r_malloc(const size_t size) {
-	void * result = malloc(size);
-	assert(result != NULL);
-	return result;
+    void * result = malloc(size);
+    assert(result != NULL);
+    return result;
 }
 
 extern void * r_calloc(const size_t n, const size_t size) {
-	void * result = calloc(n, size);
-	assert(result != NULL);
-	return result;
+    void * result = calloc(n, size);
+    assert(result != NULL);
+    return result;
 }
 
 extern void * r_recalloc(void * ptr, const size_t n, const size_t size) {
-	void * result = realloc(ptr, n * size);
-	assert(result != NULL);
-	return result;
+    void * result = ptr == NULL ? calloc(n, size) : realloc(ptr, n * size);
+    assert(result != NULL);
+    return result;
 }
 
 extern void * r_realloc(void * ptr, const size_t new_size) {
-	void * result = realloc(ptr, new_size);
-	assert(result != NULL);
-	return result;
+    void * result = realloc(ptr, new_size);
+    assert(result != NULL);
+    return result;
 }
 
 extern void * r_free(void * ptr) {
-	if (ptr != NULL) {
-		free(ptr);
-	}
-	return NULL;
+    if (ptr != NULL) {
+        free(ptr);
+    }
+    return NULL;
 }
