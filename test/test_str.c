b1c547d1c72b7e001fe63a77e6dce6f1edf298a2
Matthew Hinkle <matthewhinkle@live.com>
2025-05-18 22:23:58 -0700

    str - refactor module to be comaptible with C string data.

--- test/test_str.c
+++ test/test_str.c
@@ -1,40 +1,101 @@
-//
-// Created by Matt on 5/3/2025.
-//
+/*
+ * str tests.
+ */
 
 #include "../src/str.h"
 #include "CUnit/Basic.h"
 
 #include "test.h"
 
-static void slit__should_return_str() {
-    str s = slit("hello");
-    CU_ASSERT_PTR_NOT_NULL(s.lit);
-    CU_ASSERT_STRING_EQUAL(s.lit, "hello");
-    CU_ASSERT_EQUAL(s.size, 6); // "hello" + NULL terminator
+#include <stdint.h>
+#include <stdlib.h>
+
+static const char * HELLO = "hello";
+static const size_t HELLO_LEN = 5;
+
+static void str__for_nullptr__should_return__nullptr() {
+    CU_ASSERT_PTR_NULL(str(nullptr));
 }
 
-void str_creation(void) {
-    str s = cstr("hello");
-    CU_ASSERT_PTR_NOT_NULL(s.chars);
-    CU_ASSERT_STRING_EQUAL(s.chars, "hello");
-    CU_ASSERT_EQUAL(s.size, 6); // "hello" + NULL terminator
-    sfree(&s);
+static void str__for_empty_cstr__should_return__nonnull() {
+    const char * s = str("");
+    CU_ASSERT_PTR_NOT_NULL(s);
+    str_free(s);
 }
 
-void str_concatenation(void) {
-    str s = cstr("hello");
-    sadd(&s, cstr(" world"));
-    CU_ASSERT_STRING_EQUAL(s.chars, "hello world");
-    CU_ASSERT_EQUAL(s.size, 12); // "hello world" + NULL terminator
-    sfree(&s);
+static void str__for_empty_cstr__should_return__first_byte_of_cstr_data() {
+    const char * s = str("");
+    CU_ASSERT_STRING_EQUAL(s, "");
+    str_free(s);
 }
 
-void str_free(void) {
-    str s = cstr("test");
-    sfree(&s);
-    CU_ASSERT_PTR_NULL(s.chars);
-    CU_ASSERT_EQUAL(s.size, 0);
+static void str__for_empty_cstr__should_return__empty_cstr() {
+    const char * s = str("");
+    CU_ASSERT_EQUAL(strlen(s), 0);
+    str_free(s);
+}
+
+static void str__for_empty_cstr__should_return__cstr_with_nullterm() {
+    const char * s = str("");
+    CU_ASSERT_EQUAL(s[0], NULLTERM);
+    str_free(s);
+}
+
+static void str__for_empty_cstr__should_return__cstr_within_buffer_starting_with_SOH() {
+    const char * s = str("");
+
+    // SOH | metadata | STX | c string ...
+    const size_t header_size = 1 + sizeof(uint64_t) + 1;
+    CU_ASSERT_EQUAL(*(s - header_size), 0x01); // SOH - "start of header"
+
+    str_free(s);
+}
+
+static void str__for_empty_cstr__should_return__cstr_within_buffer_with_empty_size() {
+    const char * s = str("");
+
+    //  ... | metadata | STX | c string ...
+    const size_t size_loc = sizeof(uint64_t) + 1;
+    uint64_t val = 0;
+    memcpy(&val, s - size_loc, sizeof(uint64_t));
+
+    // size should be exactly 1, representing only the null term
+    CU_ASSERT_EQUAL(val, 1);
+
+    str_free(s);
+}
+
+static void str__for_empty_cstr__should_return__cstr_within_buffer_after_STX() {
+    const char * s = str("");
+
+    //  ... | STX | c string ...
+    CU_ASSERT_EQUAL(*(s - 1), 0x02); // STX - "start of text"
+
+    str_free(s);
+}
+
+static void str__for_nonempty_cstr__should_return__nonnull() {
+    const char * s = str(HELLO);
+    CU_ASSERT_PTR_NOT_NULL(s);
+    str_free(s);
+}
+
+static void str__for_nonempty_cstr__should_return__first_byte_of_cstr_data() {
+    const char * s = str(HELLO);
+    CU_ASSERT_STRING_EQUAL(s, HELLO);
+    str_free(s);
+}
+
+static void str__for_nonempty_cstr__should_return__cstr_with_same_len_as_input() {
+    const char * s = str(HELLO);
+    CU_ASSERT_EQUAL(strlen(s), HELLO_LEN);
+    str_free(s);
+}
+
+static void str__for_nonempty_cstr__should_return__cstr_with_nullterm() {
+    const char * s = str(HELLO);
+    CU_ASSERT_EQUAL(s[HELLO_LEN], NULLTERM);
+    str_free(s);
 }
 
 int main() {
@@ -42,19 +103,34 @@ int main() {
         return CU_get_error();
     }
 
-    CU_pSuite suite = CU_add_suite("str_tests", 0, 0);
-    if (NULL == suite) {
+    // ReSharper disable once CppLocalVariableMayBeConst
+    CU_pSuite suite = CU_add_suite("str_tests", nullptr, nullptr);
+    if (suite == nullptr) {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-    ADD_TEST(suite, slit__should_return_str);
-    ADD_TEST(suite, str_creation);
-    ADD_TEST(suite, str_concatenation);
-    ADD_TEST(suite, str_free);
+    ADD_TEST(suite, str__for_nullptr__should_return__nullptr);
+
+    ADD_TEST(suite, str__for_empty_cstr__should_return__nonnull);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__first_byte_of_cstr_data);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__empty_cstr);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__cstr_with_nullterm);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__cstr_within_buffer_starting_with_SOH);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__cstr_within_buffer_with_empty_size);
+    ADD_TEST(suite, str__for_empty_cstr__should_return__cstr_within_buffer_after_STX);
+
+    ADD_TEST(suite, str__for_nonempty_cstr__should_return__nonnull);
+    ADD_TEST(suite, str__for_nonempty_cstr__should_return__first_byte_of_cstr_data);
+    ADD_TEST(suite, str__for_nonempty_cstr__should_return__cstr_with_same_len_as_input);
+    ADD_TEST(suite, str__for_nonempty_cstr__should_return__cstr_with_nullterm);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return CU_get_error();
 }
+
+#ifdef RUN_TESTS
+    #undef RUN_TESTS
+#endif // RUN_TESTS
