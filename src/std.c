46ec7c6d7e0afaac5f7fe33e8427157ddb110751
Matthew Hinkle <matthewhinkle@live.com>
2025-05-28 19:46:19 -0700

    Add `strf` to support creating strings using format args. Update types in coll.h and std.c to align with C23.

--- src/std.c
+++ src/std.c
@@ -9,25 +9,25 @@
 
 extern void * r_malloc(const size_t size) {
     void * result = malloc(size);
-    assert(result != NULL);
+    assert(result != nullptr);
     return result;
 }
 
 extern void * r_calloc(const size_t n, const size_t size) {
     void * result = calloc(n, size);
-    assert(result != NULL);
+    assert(result != nullptr);
     return result;
 }
 
 extern void * r_recalloc(void * ptr, const size_t n, const size_t size) {
-    void * result = ptr == NULL ? calloc(n, size) : realloc(ptr, n * size);
-    assert(result != NULL);
+    void * result = ptr == nullptr ? calloc(n, size) : realloc(ptr, n * size);
+    assert(result != nullptr);
     return result;
 }
 
 extern void * r_realloc(void * ptr, const size_t new_size) {
     void * result = realloc(ptr, new_size);
-    assert(result != NULL);
+    assert(result != nullptr);
     return result;
 }
 
